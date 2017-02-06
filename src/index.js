ef3c3af6607f03453573cd226cb152a2944ae89c
twobin <twobin@live.com>
2017-02-06 10:57:09 +0800

    fix:update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import loaderUtils from 'loader-utils';
 
 module.exports = function(scss) {
-  const query = loaderUtils.parseQuery(this.query);
-  const { cardId } = query;
+  // const query = loaderUtils.parseQuery(this.query);
 
-  if (cardId) {
-    return '#' + cardId + ' {' + scss + '}';
+  let cardId;
+
+  if (this.resourcePath) {
+    const pathArray = this.resourcePath.replace(/^\//,'').split('/') || [];
+
+    if (pathArray.length && pathArray[pathArray.length - 1] && pathArray[pathArray.length - 1].indexOf('scss') !== -1) {
+      cardId = pathArray.length - 2 ? pathArray[pathArray.length - 2] : null;
+    }
+
+    if (cardId) {
+      return '#Oneness_' + cardId + ' {' + scss + '}';
+    }
+
+    return scss;
   }
 
-  return '.remoteComponent {' + scss + '}';
+  return scss;
 };
