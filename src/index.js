3b109147792f2862e7280ede0ba83eca06d836c8
Cam Song <neosoyn@gmail.com>
2017-04-25 09:16:39 +0800

    feat: support :global {} style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 import loaderUtils from 'loader-utils';
 
+function analyse(code) {
+  let open = 1;
+  let closedIndex = -1;
+  const indexOfGlobal = code.indexOf(':global {');
+  if (indexOfGlobal === -1) {
+    return {
+      localPart: code,
+      globalPart: '',
+    };
+  }
+  for (let i = indexOfGlobal + 9; i < code.length; i++) {
+    if (code[i] === '{') {
+      open += 1;
+    } else if (code[i] === '}') {
+      open -= 1;
+    }
+    if(open === 0) {
+      closedIndex = i;
+      return {
+        localPart: code.substring(0, indexOfGlobal) + code.substring(closedIndex + 1, code.length + 1),
+        globalPart: code.substring(indexOfGlobal, closedIndex + 1)
+    };
+    }
+  }
+
+  return {
+    localPart: code,
+    globalPart: '',
+  };
+}
+
 module.exports = function(scss) {
-  // const query = loaderUtils.parseQuery(this.query);
+  const { localPart, globalPart } = analyse(scss);
 
   let cardId;
 
@@ -13,7 +44,7 @@ module.exports = function(scss) {
     }
 
     if (cardId) {
-      return '.onenessCard-' + cardId + ' {' + scss + '}';
+      return '.onenessCard-' + cardId + ' {' + localPart + '}' + globalPart;
     }
 
     return scss;
